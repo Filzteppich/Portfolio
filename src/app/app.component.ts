71253305c5a6c46321784f7322a43d9fb2e9cac9
Filzteppich <khang_136@yahoo.de>
2025-12-16 17:44:02 +0100

    feat: Revamp contact section layout and styles, enhance landing page content, and update global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./footer/footer.component";
-import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, MainContentComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
