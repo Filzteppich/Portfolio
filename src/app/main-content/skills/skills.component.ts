e425041a9e0797cdf904eec477b9e5ec64ea6e6b
Filzteppich <khang_136@yahoo.de>
2025-12-17 00:03:16 +0100

    Add hover underline animation and update footer and landing page styles
    - Created a new SCSS file for hover underline animation and applied it to navbar links.
    - Enhanced footer styles to include pointer cursor on hover for social icons.
    - Updated landing page layout with improved alignment and spacing.
    - Adjusted padding and positioning for better visual hierarchy.
    - Replaced SVG icons with PNG versions for skills section and removed unused SVG files.
    - Added new logo icons for various technologies in the assets directory.

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -11,25 +11,25 @@ import { Component } from '@angular/core';
 export class SkillsComponent {
   skills = [{
     name: 'HTML',
-    image: '/assets/icons/skill-icons/html.svg'
+    image: '/assets/icons/skill-icons/html-logo.webp'
   },{
     name: 'CSS',
-    image: '/assets/icons/skill-icons/css.svg'
+    image: '/assets/icons/skill-icons/css-logo.png'
   },{
     name: 'JavaScript',
-    image: '/assets/icons/skill-icons/javaScript.svg'
+    image: '/assets/icons/skill-icons/javascript-logo.png'
   },{
     name: 'TypeScript',
-    image: '/assets/icons/skill-icons/typescript.svg'
+    image: '/assets/icons/skill-icons/typescript-logo.png'
   },{
     name: 'Angular',
-    image: '/assets/icons/skill-icons/angular.svg'
+    image: '/assets/icons/skill-icons/angular-logo.png'
   },{
     name: 'Firebase',
-    image: '/assets/icons/skill-icons/firebase.svg'
+    image: '/assets/icons/skill-icons/firebase-logo.png'
   },{
     name: 'Git',
-    image: '/assets/icons/skill-icons/git.svg'
+    image: '/assets/icons/skill-icons/git-icon.svg.png'
   },{
     name: 'Rest API',
     image: '/assets/icons/skill-icons/api.svg'
@@ -38,6 +38,6 @@ export class SkillsComponent {
     image: '/assets/icons/skill-icons/materialDesign.svg'
   },{
     name: 'Continually learning',
-    image: '/assets/icons/skill-icons/continually-learning.svg'
+    image: '/assets/icons/skill-icons/growth-icon.png'
   }]
 }
